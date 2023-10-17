2fc2d100b8d709933546d95eaa6499141b35d4d0
Nakiyama1234 <99705929+Nakiyama1234@users.noreply.github.com>
2023-10-17 08:59:50 +0700

    DI-Fitur Zakat

--- app/src/main/java/com/naufal/idnsolo/dashboardislmai/zakat/ZakatActivity.kt
+++ app/src/main/java/com/naufal/idnsolo/dashboardislmai/zakat/ZakatActivity.kt
@@ -1,54 +1,61 @@
 package com.naufal.idnsolo.dashboardislmai.zakat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.naufal.idnsolo.dashboardislmai.R
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.naufal.idnsolo.dashboardislmai.databinding.ActivityZakatBinding
 import java.text.NumberFormat
 import java.util.Locale
 
 class ZakatActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityZakatBinding
+    private lateinit var binding: ActivityZakatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityZakatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
         initHitungZakat()
 
     }
 
     private fun initHitungZakat() {
-        var isEmpty = false
-
         binding.btnHitung.setOnClickListener {
             var isEmptyField = false
-            val totalHarta = binding.totalHartaAll.text.trim().toString()
+            val localeID = Locale("in", "ID")
+            val formatRupiah: NumberFormat = NumberFormat.getCurrencyInstance(localeID)
+            var inputZakat = binding.totalHartaAll.text.toString().trim()
 
-            if (totalHarta.isEmpty()) {
+            if (inputZakat.isEmpty()) {
                 isEmptyField = true
                 binding.totalHartaAll.error = "Fild ini tidak boleh kosong"
             }
 
             if (!isEmptyField) {
-                val emas = 92395000
-                val persen = 0.025
-                val localeID = Locale("in", "ID")
-                val formatRupiah: NumberFormat = NumberFormat.getCurrencyInstance(localeID)
-                val totalHarta= binding.totalHartaAll.text.toString().trim()
-
-                if (totalHarta.isEmpty()) {
-                    isEmptyField = true
-                    binding.status.text = "Belum wajib zakat"
-
+                if (inputZakat.toInt()!! < 92395000) {
+                    binding.TotalHartaKeseluruhan.text = formatRupiah.format(inputZakat.toInt()!!)
+                    binding.status.text = "Tidak Wajib Zakat"
+                    binding.zakatYangDikeluarkan.text = formatRupiah.format(0)
+                    Toast.makeText(this, "Tidak Wajib Zakat", Toast.LENGTH_SHORT).show()
                 } else {
-                    val zakat = totalHarta.toDouble() * persen
-                    binding.status.text = "Wajib Zakat"
-                    binding.zakatYangDikeluarkan.text = formatRupiah.format(zakat)
+                    //rumus zakat
+                    //total harta / 2.5% = zakat yang di keluarkan
+                    //total harta / 0.025 = zakat di keluarkanz
+                    binding.zakatYangDikeluarkan.text = formatRupiah.format(
+                        binding.totalHartaAll.text.toString().toDouble()!! * 0.025
+                    )
+                    Toast.makeText(this, "Wajib Zakat", Toast.LENGTH_SHORT).show()
                 }
-
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
