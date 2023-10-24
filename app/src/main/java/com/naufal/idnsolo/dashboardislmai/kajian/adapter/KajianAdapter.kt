adcb18be09d880b1947106f0414c4e9a2c82ef64
Nakiyama1234 <99705929+Nakiyama1234@users.noreply.github.com>
2023-10-24 09:24:00 +0700

    DI-Fitur Kajian

--- app/src/main/java/com/naufal/idnsolo/dashboardislmai/kajian/adapter/KajianAdapter.kt
+++ app/src/main/java/com/naufal/idnsolo/dashboardislmai/kajian/adapter/KajianAdapter.kt
@@ -1,4 +1,37 @@
 package com.naufal.idnsolo.dashboardislmai.kajian.adapter
 
-class KajianAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.naufal.idnsolo.dashboardislmai.R
+import com.naufal.idnsolo.dashboardislmai.kajian.model.KajianModel
+
+class KajianAdapter(private val listkajian: ArrayList<KajianModel>) :
+    RecyclerView.Adapter<KajianAdapter.ListViewHolder>() {
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imgThumbnail: ImageView = itemView.findViewById(R.id.thumbnail)
+        var tvPenceramah: TextView = itemView.findViewById(R.id.tv_penceramah)
+        var tvChannel: TextView = itemView.findViewById(R.id.tv_channel_youtube)
+        var tvJudul: TextView = itemView.findViewById(R.id.judul_kajian)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_row_doa, parent, false)
+
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listkajian.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        var kajian = listkajian[position]
+
+
+    }
 }
